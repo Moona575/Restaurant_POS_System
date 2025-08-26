5f2480cf43938b5c930a3c071521557cad8e31d3
Moona575 <mehmoonairfan57@gmail.com>
2025-08-27 03:37:09 +0500

    fhfuwa

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -8,7 +8,13 @@ const categoryRoutes = require("./routes/categoryRoutes");
 const dishRoutes = require("./routes/dishRoutes");
 
 const app = express();
-const PORT = process.env.PORT || config.port;
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+const PORT = process.env.PORT;
 
 // CORS setup
 const corsOptions = {
