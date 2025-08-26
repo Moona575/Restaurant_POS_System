c519f7022c3962caab6b381568ee7424179a3c4d
Moona575 <mehmoonairfan57@gmail.com>
2025-08-26 22:53:16 +0500

    commented routes

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -29,7 +29,7 @@ app.get("/", (req,res) => {
 })
 
 // Other Endpoints
-app.use("/api/user", require("./routes/userRoute"));
+//app.use("/api/user", require("./routes/userRoute"));
 app.use("/api/order", require("./routes/orderRoute"));
 app.use("/api/table", require("./routes/tableRoute"));
 app.use("/api/payment", require("./routes/paymentRoute"));
