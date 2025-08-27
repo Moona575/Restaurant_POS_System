40d1c385aa549955bd7539b0c589e048c34c89d5
Moona575 <mehmoonairfan57@gmail.com>
2025-08-27 19:25:58 +0500

    again

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -43,8 +43,8 @@ app.use("/api/user", require("./routes/userRoute"));
 app.use("/api/order", require("./routes/orderRoute"));
 app.use("/api/table", require("./routes/tableRoute"));
 app.use("/api/payment", require("./routes/paymentRoute"));
-app.use("/api/categories", categoryRoutes);
-app.use("/api/dishes", dishRoutes);
+app.use("/api/categories", require("./routes/categoryRoutes"))
+app.use("/api/dishes", require("./routes/dishRoutes"))
 
 // Global Error Handler
 app.use(globalErrorHandler);
